339057c8f8c3003fc124a0745269721537c1aad6
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-24 04:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-function theBeatlesPlay(musicians, instruments) {
-  var strings = []
-  for (var i = 0; i < musicians.length; i++) {
-    strings.push(musicians[i] + " plays " + instruments[i])
-  }
-  return strings
-}
-function johnLennonFacts(facts) {
-var array = []
-let i = 0
-while (i < facts.length){
-  array.push(facts[i] + "!!!")
-}
-  return array;
-}
-function iLoveTheBeatles(n) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-   n++;
-  } while (n < 15)
-  return array;
-}
++function theBeatlesPlay (musicians, instruments) {
++  var array = [];
++  for ( var i = 0; i < musicians.length; i++) {
++    array.push(`${musicians[i]} plays ${instruments[i]}`);
++  }
++  return array;
++}
++
++function johnLennonFacts(array) {
++  const facts = [];
++  let i = 0;
++ while (i < array.length) {
++   facts.push(`${array[i]}!!!`)
++   i++;
++   }
++ return facts;
++}
++
++function iLoveTheBeatles (n) {
++  var ten = [];
++  do {
++ten.push("I love the Beatles!");
++n++
++  } while (n < 15 )
++  
++ return ten;
