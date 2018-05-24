4d3da9c0e958fdde07e1d1f17e1adec7beb2d464
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-24 04:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-+function theBeatlesPlay (musicians, instruments) {
-+  var array = [];
-+  for ( var i = 0; i < musicians.length; i++) {
-+    array.push(`${musicians[i]} plays ${instruments[i]}`);
-+  }
-+  return array;
-+}
-function johnLennonFacts(facts){
-let i = 09 dsxi
-+  var array = [];
-+  while (i < facts.length) {
-+    array.push(facts[i++] + "!!!");
-+  }
-+  return array;
+function theBeatlesPlay (musicians, instruments) {
+ var array = [];
+  for ( var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
 
-+
-+function iLoveTheBeatles (n) {
-+  var ten = [];
-+  do {
-+ten.push("I love the Beatles!");
-+n++
-+  } while (n < 15 )
-+  
-+ return ten;
+function johnLennonFacts(array) {
+  var facts = [];
+  let i = 0;
+ while (i < array.length) {
+   facts.push(`${array[i]}!!!`)
+   i++;
+   }
+ return facts;
+}
+
+function iLoveTheBeatles (n) {
+  var love = [];
+  do {
+love.push("I love the Beatles!");
+n++
+  } while (n < 15 )
+  
+ return love;
+  } 
